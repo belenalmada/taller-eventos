a908014fec5a8b33c6b1310731d49a5cb365e467
BelenAlmada <belenaf@vera.com.uy>
2024-09-07 23:48:51 -0300

    Evento
    Evento

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const miDiv = document.getElementById('miDiv');
+    miDiv.addEventListener('click', () => {
+        alert('Hola! Soy el div');
+    });
+});
