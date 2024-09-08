75667638e957fd355124cab056f81fe011b1aa45
belenalmada <belenaf@vera.com.uy>
2024-09-08 00:01:32 -0300

    solucion click js

--- eventos.js
+++ eventos.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const miDiv = document.getElementById('miDiv');
+    const boton = document.querySelector('button');
+    
     miDiv.addEventListener('click', () => {
         alert('Hola! Soy el div');
+    });
+    
+    boton.addEventListener('click', (event) => {
+        event.stopPropagation();
+        alert('Hola!');
     });
 });
